a419178963ca33bc3030295ee5e5122bf737be08
Abhijith-E <20eabhijith@gmail.com>
2026-02-27 00:34:06 +0530

    feat: implement servo angle control demonstration for robotics workshop

---
+++ ServoAngleControl.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+Servo myServo;
+
+void setup() {
+  myServo.attach(6);   // Connect servo signal to pin 6
+}
+
+void loop() {
+
+  myServo.write(0);    // Move to 0 degrees
+  delay(1000);
+
+  myServo.write(90);   // Move to 90 degrees
+  delay(1000);
+
+  myServo.write(180);  // Move to 180 degrees
+  delay(1000);
+}
